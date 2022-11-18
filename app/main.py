77a512504dbf42a8c9f2534148409bec08ae61f6
Agustin Gabrielli <agusgabrielli9@gmail.com>
2022-11-18 09:51:57 -0300

    Conditionally show /reset_db based on ENABLE_RESET env var + added trips db reset

--- app/main.py
+++ app/main.py
@@ -27,16 +27,22 @@ app.include_router(users_routes.router, tags=["Users BE"])
 app.include_router(authorization_routes.router, tags=["Auth"])
 app.include_router(trips_routes.router, tags=["Trips"])
 
+ENABLE_RESET = (os.getenv('ENABLE_RESET', 'False') == 'True')
 
-@app.delete("/reset_db", status_code=status.HTTP_200_OK)
-def reset_database():
-    users_base_url = os.getenv('USERS_BASE_URL')
-    wallets_url_base = os.getenv('WALLETS_BASE_URL')
+if ENABLE_RESET:
+    @app.delete("/reset_db", status_code=status.HTTP_200_OK)
+    def reset_database():
+        users_base_url = os.getenv('USERS_BASE_URL')
+        wallets_url_base = os.getenv('WALLETS_BASE_URL')
+        trips_url_base = os.getenv('TRIPS_BASE_URL')
 
-    url = users_base_url + "/reset_db"
-    requests.delete(url=url)
+        url = users_base_url + "/reset_db"
+        requests.delete(url=url)
 
-    url = wallets_url_base + "/reset_db"
-    requests.delete(url=url)
+        url = wallets_url_base + "/reset_db"
+        requests.delete(url=url)
 
-    return "Successfully reset"
+        url = trips_url_base + "/reset_db"
+        requests.delete(url=url)
+
+        return "Successfully reset"
